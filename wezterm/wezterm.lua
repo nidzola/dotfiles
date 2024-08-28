58efbfa4166a1c15ef2094b62e68e9412a373991
zivkovicn <x-nikola.zivkovic@transcarent.ai>
2024-08-28 23:20:55 +0200

    wezterm tidy

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,68 +1,39 @@
 local wezterm = require("wezterm")
 local config = {}
 
+-- Font and window settings
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 14
-config.automatically_reload_config = true
-config.color_scheme = "Catppuccin Frappe"
-config.use_dead_keys = false
+config.window_decorations = "RESIZE"
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+
+-- Tab settings
 config.use_fancy_tab_bar = false
 config.show_tab_index_in_tab_bar = true
 config.show_tabs_in_tab_bar = true
 config.show_new_tab_button_in_tab_bar = true
 config.tab_max_width = 40
-config.freetype_load_flags = "NO_HINTING"
+
+-- Color and rendering settings
+config.color_scheme = "Catppuccin Frappe"
 config.front_end = "WebGpu"
+config.freetype_load_flags = "NO_HINTING"
 config.freetype_load_target = "Normal"
 config.freetype_render_target = "Normal"
-config.window_decorations = "RESIZE"
-config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
-}
+
+-- Miscellaneous settings
+config.automatically_reload_config = true
+config.use_dead_keys = false
+
+-- Keybindings
 config.keys = {
-	{
-		key = "v",
-		mods = "ALT",
-		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
+	{ key = "v", mods = "ALT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
 	{ key = "h", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Left") },
-	{
-		key = "p",
-		mods = "ALT",
-		action = wezterm.action.PaneSelect({
-			alphabet = "1234567890",
-		}),
-	},
-	{
-		key = "o",
-		mods = "ALT",
-		action = wezterm.action.ShowLauncherArgs({
-			flags = "FUZZY|WORKSPACES",
-		}),
-	},
-	{
-		key = "h",
-		mods = "ALT",
-		action = wezterm.action.ActivatePaneDirection("Left"),
-	},
-	{
-		key = "k",
-		mods = "ALT",
-		action = wezterm.action.ActivatePaneDirection("Up"),
-	},
-	{
-		key = "j",
-		mods = "ALT",
-		action = wezterm.action.ActivatePaneDirection("Down"),
-	},
-	{
-		key = "l",
-		mods = "ALT",
-		action = wezterm.action.ActivatePaneDirection("Right"),
-	},
+	{ key = "p", mods = "ALT", action = wezterm.action.PaneSelect({ alphabet = "1234567890" }) },
+	{ key = "o", mods = "ALT", action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
+	{ key = "k", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Up") },
+	{ key = "j", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Down") },
+	{ key = "l", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Right") },
 	{
 		key = "n",
 		mods = "ALT",
@@ -73,12 +44,7 @@ config.keys = {
 			}),
 			action = wezterm.action_callback(function(window, pane, line)
 				if line then
-					window:perform_action(
-						wezterm.action.SwitchToWorkspace({
-							name = line,
-						}),
-						pane
-					)
+					window:perform_action(wezterm.action.SwitchToWorkspace({ name = line }), pane)
 				end
 			end),
 		}),
@@ -97,22 +63,18 @@ config.keys = {
 	},
 }
 
+-- Event handlers
 wezterm.on("format-tab-title", function(tab)
 	local cwd = string.match(tab.active_pane.current_working_dir.file_path, "/([^/]*)$")
-	return {
-		{ Text = " " .. cwd .. "  " },
-	}
+	return { { Text = " " .. cwd .. "  " } }
 end)
 
 wezterm.on("update-right-status", function(window)
-	window:set_right_status(wezterm.format({
-		{ Text = "Workspace: " .. window:active_workspace() .. " " },
-	}))
+	window:set_right_status(wezterm.format({ { Text = "Workspace: " .. window:active_workspace() .. " " } }))
 end)
-local mux = wezterm.mux
 
 wezterm.on("gui-startup", function(cmd)
-	local _, _, window = mux.spawn_window(cmd or {})
+	local _, _, window = wezterm.mux.spawn_window(cmd or {})
 	window:gui_window():maximize()
 end)
 
