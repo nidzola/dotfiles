e60d4a8eb2c45c5868ec98b87adf3a593d81317c
zivkovicn <x-nikola.zivkovic@transcarent.ai>
2024-09-03 18:18:57 +0200

    fzf-lua-preview

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -6,10 +6,17 @@ return {
         vertical = "down:65%",
         layout = "vertical",
       },
+      backdrop = 100,
     },
     files = {
       git_icons = false,
       file_icons = true,
+    },
+    lsp = {
+      code_actions = {
+        -- was to slow, so disabled
+        previewer = false,
+      },
     },
   },
 }
