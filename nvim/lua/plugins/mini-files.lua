e1c2971d18d216749bbf445a959dc1eb41aee8a9
zivkovicn <x-nikola.zivkovic@transcarent.ai>
2024-09-10 15:55:35 +0200

    mini files preview

--- nvim/lua/plugins/mini-files.lua
+++ nvim/lua/plugins/mini-files.lua
@@ -3,12 +3,10 @@ return {
   opts = {
     windows = {
       preview = true,
-      width_focus = 30,
-      width_preview = 30,
+      width_focus = 40,
+      width_preview = 100,
     },
     options = {
-      -- Whether to use for editing directories
-      -- Disabled by default in LazyVim because neo-tree is used for that
       use_as_default_explorer = true,
     },
   },
