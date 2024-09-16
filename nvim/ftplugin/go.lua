b656c1e93b217521b8b9922e10fe79cbe94aa8df
zivkovicn <x-nikola.zivkovic@transcarent.ai>
2024-09-16 19:26:46 +0200

    mini split

--- nvim/ftplugin/go.lua
+++ nvim/ftplugin/go.lua
@@ -4,10 +4,3 @@ vim.api.nvim_set_keymap(
   "<Cmd>!go generate %<CR>",
   { noremap = true, silent = true, desc = "Go generate" }
 )
-
-vim.api.nvim_set_keymap(
-  "v",
-  "<leader>cg",
-  "<Cmd>QuickType<CR>",
-  { noremap = true, silent = true, desc = "Convert JSON to Go struct" }
-)
--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,30 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local MiniFiles = require("mini.files")
+
+local map_split = function(buf_id, lhs, direction)
+  local rhs = function()
+    -- Make new window and set it as target
+    local new_target_window
+    vim.api.nvim_win_call(MiniFiles.get_explorer_state().target_window, function()
+      vim.cmd(direction .. " split")
+      new_target_window = vim.api.nvim_get_current_win()
+    end)
+
+    MiniFiles.set_target_window(new_target_window)
+  end
+
+  -- Adding `desc` will result into `show_help` entries
+  local desc = "Split " .. direction
+  vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+end
+vim.api.nvim_create_autocmd("User", {
+  pattern = "MiniFilesBufferCreate",
+  callback = function(args)
+    local buf_id = args.data.buf_id
+    map_split(buf_id, "gs", "belowright horizontal")
+    map_split(buf_id, "gv", "belowright vertical")
+  end,
+})
