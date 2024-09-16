e2b7e219f0e735d660f312608638c50d6ce18bd6
zivkovicn <x-nikola.zivkovic@transcarent.ai>
2024-09-16 14:38:10 +0200

    remove workspace gen

--- nvim/ftplugin/go.lua
+++ nvim/ftplugin/go.lua
@@ -11,15 +11,3 @@ vim.api.nvim_set_keymap(
   "<Cmd>'<,'>!node ~/.config/nvim/utils/json-to-go.js<CR>",
   { noremap = true, silent = true, desc = "Convert JSON to Go struct" }
 )
-
-_G.go_workspaces_generated = false
-
-function generate_go_workspaces()
-  if not _G.go_workspaces_generated then
-    vim.fn.system("sh ~/projects/bin/gowgen")
-    vim.notify("Go workspaces generated!", vim.log.levels.INFO)
-    _G.go_workspaces_generated = true
-  end
-end
-
-generate_go_workspaces()
