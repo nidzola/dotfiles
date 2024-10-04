cfaf3dca87ad8cb864edbdf21e593c295c08a16a
zivkovicn <x-nikola.zivkovic@transcarent.ai>
2024-10-04 19:20:05 +0200

    fix

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -1,23 +1,25 @@
 return {
   "ibhagwan/fzf-lua",
-  opts = {
-    winopts = {
-      preview = {
-        vertical = "down:65%",
-        layout = "vertical",
+  opts = function(_, opts)
+    return vim.tbl_deep_extend("force", opts, {
+      winopts = {
+        preview = {
+          vertical = "down:65%",
+          layout = "vertical",
+        },
+        backdrop = 100,
       },
-      backdrop = 100,
-    },
-    files = {
-      cwd_header = true,
-      git_icons = false,
-      file_icons = true,
-    },
-    lsp = {
-      code_actions = {
-        -- was to slow, so disabled
-        previewer = false,
+      files = {
+        cwd_header = true,
+        git_icons = false,
+        file_icons = true,
       },
-    },
-  },
+      lsp = {
+        code_actions = {
+          -- was to slow, so disabled
+          previewer = false,
+        },
+      },
+    })
+  end,
 }
