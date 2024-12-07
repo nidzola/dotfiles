b4ed8895d279b96cef6113b9155d430cd713abbb
zivkovicn <x-nikola.zivkovic@transcarent.ai>
2024-12-07 22:56:48 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install neovim exa bat fzf ripgrep fd zsh zsh-completions zsh-syntax-highlighting zsh-autosuggestions neovim git fzf mas
