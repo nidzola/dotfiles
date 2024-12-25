b759507e85a8947165db3c96e348a45e61ca78a9
zivkovicn <x-nikola.zivkovic@transcarent.ai>
2024-12-25 23:51:31 +0100

    statusline

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -2,41 +2,44 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
-  -- opts = function()
-  --   -- Toggle the profiler
-  --   Snacks.toggle.profiler():map("<leader>pp")
-  --   -- Toggle the profiler highlights
-  --   Snacks.toggle.profiler_highlights():map("<leader>ph")
-  -- end,
-  -- keys = {
-  --   {
-  --     "<leader>ps",
-  --     function()
-  --       Snacks.profiler.scratch()
-  --     end,
-  --     desc = "Profiler Scratch Buffer",
-  --   },
-  -- },
-  opts = {
-    bigfile = { enabled = true },
-    dashboard = { enabled = true },
-    indent = {
-      enabled = true,
-      animate = {
-        enabled = false,
+  opts = function()
+    local options = {
+      bigfile = { enabled = true },
+      dashboard = { enabled = true },
+      indent = {
+        enabled = true,
+        animate = {
+          enabled = false,
+        },
       },
+      lazygit = { enabled = true },
+      input = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scroll = { enabled = false },
+      statuscolumn = { enabled = false },
+      scratch = {
+        enabled = true,
+        minimal = true,
+      },
+      words = { enabled = false },
+      zen = { enabled = false },
+    }
+
+    -- Toggle the profiler
+    Snacks.toggle.profiler():map("<leader>pp")
+    -- Toggle the profiler highlights
+    Snacks.toggle.profiler_highlights():map("<leader>ph")
+
+    return options
+  end,
+  keys = {
+    {
+      "<leader>ps",
+      function()
+        Snacks.profiler.scratch()
+      end,
+      desc = "Profiler Scratch Buffer",
     },
-    lazygit = { enabled = true },
-    input = { enabled = true },
-    notifier = { enabled = true },
-    quickfile = { enabled = true },
-    scroll = { enabled = false },
-    statuscolumn = { enabled = false },
-    scratch = {
-      enabled = true,
-      minimal = true,
-    },
-    words = { enabled = false },
-    zen = { enabled = false },
   },
 }
