0fc6c9660dba1268a3094f4b3da0ba20f31e0f5c
zivkovicn <x-nikola.zivkovic@transcarent.ai>
2024-12-25 10:11:18 +0100

    blink update

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,25 +1,64 @@
 return {
   "saghen/blink.cmp",
+  dependencies = {
+    "onsails/lspkind.nvim",
+  },
+  version = "*",
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
   opts = {
+    appearance = {
+      use_nvim_cmp_as_default = false,
+      nerd_font_variant = "mono",
+    },
     completion = {
       ghost_text = {
         enabled = false,
       },
+      accept = { auto_brackets = { enabled = true } },
+
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 250,
+        treesitter_highlighting = true,
+        window = { border = "rounded" },
+      },
+
       menu = {
+        border = "rounded",
         draw = {
-          columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } },
+          columns = { { "label", gap = 1 }, { "kind_icon", "kind", gap = 1 } },
+          components = {
+            kind_icon = {
+              text = function(item)
+                local kind = require("lspkind").symbol_map[item.kind] or ""
+                return kind
+              end,
+              highlight = "CmpItemKind",
+            },
+          },
         },
       },
     },
-    sources = {
-      default = { "lsp", "buffer", "path" },
-    },
+
     keymap = {
-      preset = "enter",
       ["<Tab>"] = {
         LazyVim.cmp.map({ "ai_accept" }),
         "fallback",
       },
+    },
+
+    sources = {
+      default = { "lsp", "path", "buffer" },
+      -- disable cmd completion
+      cmdline = {},
+      -- disable snippets
+      transform_items = function(_, items)
+        return vim.tbl_filter(function(item)
+          return item.kind ~= require("blink.cmp.types").CompletionItemKind.Snippet
+        end, items)
+      end,
     },
   },
 }
