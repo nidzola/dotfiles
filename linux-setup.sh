806fe35c52dbfc258892ecbc1f56b02b78d58fb9
Nikola Zivkovic <x-nikola.zivkovic@transcarent.ai>
2025-03-27 00:46:22 +0000

    convert to arch

--- linux-setup.sh
+++ linux-setup.sh
@@ -2,13 +2,13 @@
 
 set -e # Exit immediately if a command exits with a non-zero status
 
-# Update and upgrade system
+# Update system
 echo "Updating system..."
-sudo apt update && sudo apt upgrade -y
+sudo pacman -Syu --noconfirm
 
 # Install XFCE minimal
 echo "Installing XFCE minimal..."
-sudo apt install -y xfce4 xfce4-terminal lightdm
+sudo pacman -S --noconfirm xfce4 xfce4-terminal lightdm lightdm-gtk-greeter
 
 # Enable lightdm service
 echo "Enabling LightDM..."
@@ -16,19 +16,15 @@ sudo systemctl enable lightdm
 
 # Install essential packages
 echo "Installing essential packages..."
-sudo apt install -y git curl wget zsh neovim tmux build-essential htop fzf ripgrep rsync unzip
+sudo pacman -S --noconfirm git curl wget zsh neovim tmux base-devel htop fzf ripgrep rsync unzip yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide imagemagick
 
 # Install Zsh plugins
 echo "Installing Zsh plugins..."
-sudo apt install -y zsh-syntax-highlighting zsh-autosuggestions
-
-# Install Zoxide (smart directory jumper)
-echo "Installing Zoxide..."
-curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+sudo pacman -S --noconfirm zsh-syntax-highlighting zsh-autosuggestions
 
 # Set up Zsh as default shell
-# echo "Setting Zsh as default shell..."
-chsh -s $(which zsh)
+echo "Setting Zsh as default shell..."
+chsh -s "$(which zsh)"
 
 # Install Oh My Zsh
 echo "Installing Oh My Zsh..."
@@ -42,21 +38,13 @@ sh -c "$(curl -fsSL https://git.io/get-zi)"
 
 # Install and configure firewall
 echo "Setting up firewall..."
-sudo apt install -y ufw
+sudo pacman -S --noconfirm ufw
 sudo ufw allow OpenSSH
 sudo ufw enable
 
 # Enable SSH service
 echo "Enabling SSH..."
-sudo systemctl enable --now ssh
-
-# Clone dotfiles if exists
-# echo "Cloning dotfiles..."
-# DOTFILES_REPO="git@github.com:nidzola/dotfiles.git"
-# if [ ! -d "$HOME/dotfiles" ]; then
-#   git clone $DOTFILES_REPO $HOME/dotfiles
-#   cd $HOME/dotfiles && ./install.sh
-# fi
+sudo systemctl enable --now sshd
 
 # Install Tailscale
 echo "Installing Tailscale..."
@@ -65,15 +53,15 @@ sudo systemctl enable --now tailscaled
 
 # Install socat (for remote port forwarding)
 echo "Installing socat..."
-sudo apt install -y socat
+sudo pacman -S --noconfirm socat
 
 # Install Node.js & npm
 echo "Installing Node.js and npm..."
-sudo apt install -y nodejs npm
+sudo pacman -S --noconfirm nodejs npm
 
 # Install Go
 echo "Installing Go..."
-sudo apt install -y golang-go
+sudo pacman -S --noconfirm go
 
 # Install Go tools
 echo "Installing Go tools..."
@@ -88,16 +76,13 @@ git clone https://github.com/tfutils/tfenv.git ~/.tfenv
 echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >>~/.zshrc
 
 # Install a specific version of Terraform
-$HOME/.tfenv/bin/tfenv install v1.6.2
-$HOME/.tfenv/bin/tfenv use v1.6.2
+"$HOME/.tfenv/bin/tfenv" install v1.6.2
+"$HOME/.tfenv/bin/tfenv" use v1.6.2
 
 # Install PostgreSQL and pgvector
 echo "Installing PostgreSQL and pgvector..."
-sudo apt install -y postgresql postgresql-client
-sudo psql --version
-sudo service postgresql status
+sudo pacman -S --noconfirm postgresql postgresql-libs
 sudo systemctl enable --now postgresql
-sudo apt install postgresql-server-dev-17
 
 rm -rf ~/pgvector
 git clone --branch v0.8.0 https://github.com/pgvector/pgvector.git ~/pgvector
@@ -109,37 +94,34 @@ rm -rf ~/pgvector
 
 # Install Redis
 echo "Installing Redis..."
-sudo apt install -y redis-server
-sudo systemctl enable --now redis-server
+sudo pacman -S --noconfirm redis
+sudo systemctl enable --now redis
 
 # Install protobuf
 echo "Installing protobuf..."
-sudo apt install -y protobuf-compiler
+sudo pacman -S --noconfirm protobuf
 
 # Install Google Cloud SDK
 echo "Installing Google Cloud SDK..."
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.gpg
-sudo apt update
-sudo apt-get install -y google-cloud-sdk
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo pacman-key --add -
+sudo pacman -S --noconfirm google-cloud-sdk
 
 echo "Other deps..."
 curl https://sh.rustup.rs -sSf | sh
-sudo apt install -y python3.12-venv
+sudo pacman -S --noconfirm python-virtualenv
 
 echo "Lazygit installation..."
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": *"v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit -D -t /usr/local/bin/
 
 # Clean up
 echo "Cleaning up..."
-sudo apt autoremove -y && sudo apt clean
+sudo pacman -Rns $(pacman -Qdtq) --noconfirm || true
 
 # Dotfiles
 echo "Dotfiles..."
-
 cp -rf ~/projects/dotfiles/linux/nvim ~/.config
 cp ~/projects/dotfiles/linux/.tmux.conf ~/
 cp ~/projects/dotfiles/linux/.zshrc ~/
