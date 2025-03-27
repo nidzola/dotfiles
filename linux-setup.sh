c0dcbfb4fd926b548fde6bc373fd0b046e95a098
Nikola Zivkovic <x-nikola.zivkovic@transcarent.ai>
2025-03-27 00:16:39 +0000

    update

--- linux-setup.sh
+++ linux-setup.sh
@@ -16,18 +16,18 @@ sudo systemctl enable lightdm
 
 # Install essential packages
 echo "Installing essential packages..."
-sudo apt install -y git curl wget zsh neovim tmux build-essential htop fzf ripgrep rsync
+sudo apt install -y git curl wget zsh neovim tmux build-essential htop fzf ripgrep rsync unzip
 
 # Install Zsh plugins
 echo "Installing Zsh plugins..."
-sudo apt install -y zsh-syntax-highlighting zsh-autosuggestions zsh-vi-mode
+sudo apt install -y zsh-syntax-highlighting zsh-autosuggestions
 
 # Install Zoxide (smart directory jumper)
 echo "Installing Zoxide..."
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 
 # Set up Zsh as default shell
-echo "Setting Zsh as default shell..."
+# echo "Setting Zsh as default shell..."
 chsh -s $(which zsh)
 
 # Install Oh My Zsh
@@ -51,12 +51,12 @@ echo "Enabling SSH..."
 sudo systemctl enable --now ssh
 
 # Clone dotfiles if exists
-echo "Cloning dotfiles..."
-DOTFILES_REPO="git@github.com:yourusername/dotfiles.git"
-if [ ! -d "$HOME/dotfiles" ]; then
-  git clone $DOTFILES_REPO $HOME/dotfiles
-  cd $HOME/dotfiles && ./install.sh
-fi
+# echo "Cloning dotfiles..."
+# DOTFILES_REPO="git@github.com:nidzola/dotfiles.git"
+# if [ ! -d "$HOME/dotfiles" ]; then
+#   git clone $DOTFILES_REPO $HOME/dotfiles
+#   cd $HOME/dotfiles && ./install.sh
+# fi
 
 # Install Tailscale
 echo "Installing Tailscale..."
@@ -81,20 +81,36 @@ go install github.com/vektra/mockery/v2@latest
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.64.8
 go install github.com/google/wire/cmd/wire@latest
 
-# Install env tools
-echo "Installing efenv..."
-git clone https://github.com/efenv/efenv.git $HOME/.efenv && cd $HOME/.efenv && make install
+# Install tfenv for managing Terraform versions
+echo "Installing tfenv..."
+rm -rf ~/.tfenv
+git clone https://github.com/tfutils/tfenv.git ~/.tfenv
+echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >>~/.zshrc
 
-# Install PostgreSQL 13 and pgvector
-echo "Installing PostgreSQL 13 and pgvector..."
-sudo apt install -y postgresql-13 postgresql-client-13 postgresql-contrib-13
+# Install a specific version of Terraform
+$HOME/.tfenv/bin/tfenv install v1.6.2
+$HOME/.tfenv/bin/tfenv use v1.6.2
+
+# Install PostgreSQL and pgvector
+echo "Installing PostgreSQL and pgvector..."
+sudo apt install -y postgresql postgresql-client
+sudo psql --version
+sudo service postgresql status
 sudo systemctl enable --now postgresql
-sudo -u postgres psql -c "CREATE EXTENSION IF NOT EXISTS vector;"
+sudo apt install postgresql-server-dev-17
+
+rm -rf ~/pgvector
+git clone --branch v0.8.0 https://github.com/pgvector/pgvector.git ~/pgvector
+cd ~/pgvector
+make
+sudo make install
+cd ..
+rm -rf ~/pgvector
 
 # Install Redis
 echo "Installing Redis..."
 sudo apt install -y redis-server
-sudo systemctl enable --now redis
+sudo systemctl enable --now redis-server
 
 # Install protobuf
 echo "Installing protobuf..."
@@ -102,20 +118,31 @@ sudo apt install -y protobuf-compiler
 
 # Install Google Cloud SDK
 echo "Installing Google Cloud SDK..."
-export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt update && sudo apt install -y google-cloud-sdk
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.gpg
+sudo apt update
+sudo apt-get install -y google-cloud-sdk
 
-# Install Terraform v1.6.2
-echo "Installing Terraform v1.6.2..."
-wget https://releases.hashicorp.com/terraform/1.6.2/terraform_1.6.2_linux_amd64.zip
-unzip terraform_1.6.2_linux_amd64.zip
-sudo mv terraform /usr/local/bin/
-rm terraform_1.6.2_linux_amd64.zip
+echo "Other deps..."
+curl https://sh.rustup.rs -sSf | sh
+sudo apt install -y python3.12-venv
+
+echo "Lazygit installation..."
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
 
 # Clean up
 echo "Cleaning up..."
 sudo apt autoremove -y && sudo apt clean
+
+# Dotfiles
+echo "Dotfiles..."
+
+cp -rf ~/projects/dotfiles/linux/nvim ~/.config
+cp ~/projects/dotfiles/linux/.tmux.conf ~/
+cp ~/projects/dotfiles/linux/.zshrc ~/
+cp ~/projects/dotfiles/lazygit ~/.config
 
 echo "Setup complete! Reboot recommended."
