e53a51316e497b743994a914e3fbaf0fa4335d76
zivkovicn <x-nikola.zivkovic@transcarent.ai>
2025-04-17 23:16:58 +0200

    main | cp

--- nvim/lua/plugins/nvim-dap.lua
+++ nvim/lua/plugins/nvim-dap.lua
@@ -2,6 +2,19 @@ return {
   "mfussenegger/nvim-dap",
   opts = function(_, opts)
     local dap = require("dap")
+
+    dap.configurations.go = dap.configurations.go or {}
+    -- table.insert(dap.configurations.go, {
+    --   type = "go",
+    --   name = "Debug with SOCKS5 Proxy",
+    --   request = "launch",
+    --   program = "${file}",
+    --   env = {
+    --     -- SOCKS5_PROXY = "192.168.50.201:1080", -- replace with your actual SOCKS5 proxy
+    --     http_proxy = "socks5h://localhost:1080",
+    --     https_proxy = "socks5h://localhost:1080",
+    --   },
+    -- })
     ----------------------------------------------------------------------------
     -- LISTEN FOR 'event_output' AND STRIP/REFORMAT LOG LINES
     ----------------------------------------------------------------------------
